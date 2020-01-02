70da36a6e6354df716633b80ea8a4707e4c8385f
Gustavo da Silva <gustavocontabeis@gmail.com>
2020-01-02 19:31:45 -0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {AccordionModule} from 'primeng/accordion';
 import {MenuItem, MessageService, ConfirmationService} from 'primeng/api';
-import { HomeComponent } from './home/home.component';                 //api
+import { HomeComponent } from './home/home.component';
 import { PessoaComponent } from './pessoa/pessoa.component';	
 import { PessoaService } from './pessoa/pessoa.service';
 import {ToastModule} from 'primeng/toast';
 import {MenubarModule} from 'primeng/menubar';
 import {PanelModule} from 'primeng/panel';
 import {TableModule} from 'primeng/table';
+import {DialogModule} from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,11 @@ import {TableModule} from 'primeng/table';
     AccordionModule,
     PanelModule,
     ToastModule,
-    MenubarModule
+    MenubarModule,
+    TableModule,
+    DialogModule,
+    FormsModule,
+    ButtonModule
   ],
   providers: [MessageService, ConfirmationService, PessoaService],
   bootstrap: [AppComponent]
