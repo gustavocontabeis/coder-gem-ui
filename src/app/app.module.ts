f526ea30ff5f44dc6ab6ee6c08ee5e396a2335fa
Gustavo da Silva <gustavocontabeis@gmail.com>
2020-01-02 17:53:19 -0300

    ok2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
-import {MenuItem} from 'primeng/api';
+import {MenuItem, MessageService, ConfirmationService} from 'primeng/api';
 import { HomeComponent } from './home/home.component';                 //api
 import { PanelModule } from 'node_modules/primeng/panel';
 import { PessoaComponent } from './pessoa/pessoa.component';	
+import { PessoaService } from './pessoa/pessoa.service';
 
 @NgModule({
   declarations: [
@@ -20,10 +22,11 @@ import { PessoaComponent } from './pessoa/pessoa.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AccordionModule,
     PanelModule
   ],
-  providers: [],
+  providers: [MessageService, ConfirmationService, PessoaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pessoa/pessoa.component.ts
+++ src/app/pessoa/pessoa.component.ts
@@ -1,4 +1,8 @@
+import { PessoaService } from './pessoa.service';
 import { Component, OnInit } from '@angular/core';
+import { Pessoa } from './pessoa';
+import { MessageService, ConfirmationService, SelectItem } from 'primeng/api';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-pessoa',
@@ -7,9 +11,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoaComponent implements OnInit {
 
-  constructor() { }
+  pessoa: Pessoa = new Pessoa();
+  pessoas: Pessoa[];
+  exibirDialog: boolean;
+  novoRegistro: boolean;
+  tiposPessoa: SelectItem[] = [];
+
+  constructor(
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private pessoaService: PessoaService) { }
 
   ngOnInit() {
+    this.exibirDialog = false;
+    this.novoRegistro = false;
+    this.consultar();
+    this.pessoa = new Pessoa();
+  }
+
+  consultar() {
+    this.pessoaService.consultar().subscribe(resposta => {
+      this.pessoas = resposta as Pessoa[];
+    }, error => {
+      console.log(error);
+      alert('erro pessoas.' + error);
+    });
+  }
+
+  novo() {
+    const pessoa = new Pessoa();
+    this.exibirModal(pessoa);
+  }
+
+  exibirModal(pessoa: Pessoa) {
+    this.novoRegistro = true;
+    this.exibirDialog = true;
+    this.pessoa = pessoa;
+  }
+
+  salvar() {
+    console.log('salvar');
+    this.pessoaService.adicionar(this.pessoa).subscribe(resposta => {
+      this.consultar();
+      this.exibirDialog = false;
+      this.novoRegistro = false;
+      this.messageService.add({severity: 'success', summary: 'OK', detail: 'Registro adicionado com sucesso.'});
+      }, error => {
+        console.log(error);
+        alert(error.ok);
+      }
+    );
+  }
+
+  confirmarExcluir() {
+    console.log('confirmarExcluir');
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja excluir este registro?',
+      accept: () => {
+          console.log('confirmarExcluir - accept');
+          this.excluir();
+      },
+      reject: () => {
+          this.messageService.add({severity: 'success', summary: 'Cancelado', detail: 'Ok. Cancelado.'});
+      }
+    });
+  }
+
+  excluir() {
+    console.log('excluir');
+    this.pessoaService.excluir(this.pessoa).subscribe(resposta => {
+      this.consultar();
+      this.exibirDialog = false;
+      this.novoRegistro = false;
+      this.messageService.add({severity: 'success', summary: 'OK', detail: 'Registro excluído com sucesso.'});
+      }, error => alert('erro pessoas.')
+    );
+  }
+
+  aoSelecionar(event) {
+    this.novoRegistro = false;
   }
 
 }
+
